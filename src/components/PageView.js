939014dcaefa0ac7b5d5daa2dcf40865818952f9
Olga Orel <olga.detterer@gmail.com>
2018-10-25 10:39:50 -0700

    changed PageView Comonent to pure function

--- src/components/PageView.js
+++ src/components/PageView.js
@@ -1,29 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Location from './Location';
 import WeekView from './WeekView';
 import HourlyView from './HourlyView';
 import DailyAddition from './DailyAddition';
 
-export default class PageView extends Component {
-
-    render() {
-        return (
-            <div className="flex justify-center ">
-                <div className="w-3/4 my-24 h-screen">
-                    <Location cityName={this.props.cityName} />
-                    <div className="border-2 rounded shadow-lg">
-                        <WeekView
-                            daily={this.props.daily} />
-                        <HourlyView
-                            hourly={this.props.hourly}
-                            daily={this.props.daily} />
-                        <DailyAddition 
-                            daily={this.props.daily} />
-                    </div>
-
+export default function PageView(props) {
+    return (
+        <div className="flex justify-center ">
+            <div className="w-3/4 my-24 h-screen">
+                <Location 
+                    cityName={props.cityName} />
+                <div className="border-2 rounded shadow-lg">
+                    <WeekView
+                        daily={props.daily} />
+                    <HourlyView
+                        hourly={props.hourly}
+                        daily={props.daily} />
+                    <DailyAddition
+                        daily={props.daily} />
                 </div>
             </div>
-
-        )
-    }
+        </div>
+    )
 }
